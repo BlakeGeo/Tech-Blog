2a926cb33d89f2e7e7bb032ac4eed5b2398b6096
Blake Georgeff <georgeffb@hotmail.com>
2021-09-20 16:51:36 +0800

    finished and updated

--- public/js/blog.js
+++ public/js/blog.js
@@ -17,7 +17,7 @@ async function newBlogHandler(event) {
     });
 
   if (response.ok) {
-    document.location.replace('/dashboard');
+    document.location.reload();
   } else {
     alert(response.statusText);
   }
--- public/js/editpost.js
+++ public/js/editpost.js
@@ -6,7 +6,7 @@ const editBlogHandler = async (event) => {
       window.location.toString().split('/').length - 1
     ];
     if (title && content) {
-      const response = await fetch(`/api/blogs/${blog_id}`, {
+      const response = await fetch(`/api/blog/${blog_id}`, {
         method: 'PUT',
         body: JSON.stringify({ title, content, blog_id }),
         headers: {
@@ -15,26 +15,27 @@ const editBlogHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace(`${blog_id}`);
+        document.location.reload();
       } else {
         alert('Failed to edit blog');
       }
     }
-  };
-  
-  
+  };  
   
   document
     .querySelector('#editbutton')
     .addEventListener('click', editBlogHandler);
   
-  const deleteBlogHandler = async (event) => {
+  
+  
+  
+    const deleteBlogHandler = async (event) => {
     event.preventDefault();
     const blog_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     {
-      const response = await fetch(`/api/blogs/${blog_id}`, {
+      const response = await fetch(`/api/blog/${blog_id}`, {
         method: 'DELETE',
         body: JSON.stringify({ blog_id }),
         headers: {
@@ -49,9 +50,7 @@ const editBlogHandler = async (event) => {
         alert('Failed to delete blog');
       }
     }
-  };
-  
-  
+  };  
   
   document
     .querySelector('#deletebutton')
